01190fe54c0f0cca07d3866cf0de59a6a24c3162
Nikolas Furtado <nikolasfurtado@gmail.com>
2022-06-18 14:08:26 -0300

    multiplication done

--- calcs.js
+++ calcs.js
@@ -1,11 +1,11 @@
-const num1 = document.getElementById("n1");
-const num2 = document.getElementById("n2");
+var num1 = document.getElementById("n1");
+var num2 = document.getElementById("n2");
+const res = document.getElementById("res");
 var myanswer = document.getElementById("answer");
-var video = document.getElementById("video");
+let video = document.getElementById("video");
 
 
 function sortnumbers() {
-
     num1.innerText = Math.floor(Math.random() * 10 + 1);
     num2.innerText = Math.floor(Math.random() * 10 + 1);
 }
@@ -13,13 +13,10 @@ function sortnumbers() {
 
 function checkanswer() {
 
-
-    Window.myanswer
-    result = num1 * num2;
-
-    num1.innerText = myanswer;
-
-
+    num1 = num1.innerText;
+    num2 = num2.innerText;
+    var result = num1 * num2;
+    res.innerText = myanswer;
 
 
 
