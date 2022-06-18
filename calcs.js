0eb078c7362b80fea28dc017fa92a8ced3e3a101
Nikolas Furtado <nikolasfurtado@gmail.com>
2022-06-18 17:03:04 -0300

    random working on button

--- calcs.js
+++ calcs.js
@@ -1,28 +1,50 @@
-var res = document.getElementById("res");
 var myanswer = document.getElementById("answer");
 var video = document.getElementById("video");
+var heart = document.getElementById("heart");
+var life = document.getElementById("resposta");
 var correct = 0;
-
-
-var num1 = document.getElementById("n1").innerText = Math.floor(Math.random() * 10 + 1);
-var num2 = document.getElementById("n2").innerText = Math.floor(Math.random() * 10 + 1);
+var num1;
+var num2;
+var movies = ["video.mp4", "video1.mp4", "video2.mp4"];
 
 function sortnumbers() {
     num1 = document.getElementById("n1").innerText = Math.floor(Math.random() * 10 + 1);
     num2 = document.getElementById("n2").innerText = Math.floor(Math.random() * 10 + 1);
-
 }
-var resp1 = num1.innerText;
-var resp2 = num2.innerText;
 
 function checkanswer() {
-
+    var resp1 = num1;
+    var resp2 = num2;
     var result = resp1 * resp2;
 
-    if (myanswer.value == result) {
-        video.play();
+
+
+
+    if (result == answer.value) {
+
+        let heartnew = '<img class="heart" src="resources/heart.png" alt=""></img>';
+        heart.insertAdjacentHTML("afterbegin", heartnew);
+
+        correct += 1;
+
+        sortnumbers();
+
+        if (correct >= 10) {
+            video.play();
+            resposta.innerText = "Parabéns! vocÊ ganhou direito a um vídeo!";
+        }
+
+
+
+
+
+    } else {
+        resposta.innerText = "Resposta Incorreta!"
+        setTimeout(function() { resposta.innerText = "" }, 700);
+
     }
 
-    sortnumbers();
+
+
 
 }
